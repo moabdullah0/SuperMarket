e5fd425178bf037f70427839ddcee674840f0529
Mohamad alibrahim <100835278+moabdullah0@users.noreply.github.com>
2024-04-02 04:57:11 +0300

    fix card home

--- src/components/Home/Navbar.tsx
+++ src/components/Home/Navbar.tsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 import logo from "../../assets/Syrian SuperMarket Logo.svg";
 import { NavbarItems } from "../../data/NavbarItems";
 import { useGlobalContext } from "../../context/language";
@@ -7,45 +7,42 @@ const Navbar = () => {
   const { mode, toggleMode } = useGlobalContext();
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const menuAnimationClasses = isOpen ? "ease-out duration-300" : "ease-in duration-300";
+  const menuAnimationClasses = isOpen
+    ? "ease-out duration-300"
+    : "ease-in duration-300";
 
   return (
     <nav className="bg-transparent bg-green-100 text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200">
       <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-        {/* Logo always visible, adjusted size for mobile */}
         <a href="#" className="flex items-center">
-          <img
-            className="h-12 w-auto" // Adjusted for responsive sizing
-            src={logo}
-            alt="Workflow"
-          />
+          <img className="h-12 w-auto" src={logo} alt="Workflow" />
         </a>
 
-        {/* Desktop Menu */}
         <div className="hidden sm:flex items-center space-x-4">
           {NavbarItems.map((nav, index) => (
             <a
               key={index}
               href={nav.url}
-              className={`text-sm font-medium px-3 py-2 rounded-md hover:bg-green-700 hover:text-white ${mode === "Arabic" && "ml-4"}`} // Adjusted spacing for RTL
+              className={`text-sm font-medium px-3 py-2 rounded-md hover:bg-green-700 hover:text-white ${
+                mode === "Arabic" && "ml-4"
+              }`} // Adjusted spacing for RTL
             >
               {mode === "Eng" ? nav.title : nav.titleArab}
             </a>
           ))}
-          {/* Toggle Button */}
+
           <div className="ml-4">
-          <select
-            onChange={toggleMode}
-            value={mode}
-            className="bg-green-50 border border-gray-300 text-green-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-          >
-            <option value="Eng">English</option>
-            <option value="Arabic">عربي</option>
-          </select>
+            <select
+              onChange={toggleMode}
+              value={mode}
+              className="bg-green-50 border border-gray-300 text-green-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+            >
+              <option value="Eng">English</option>
+              <option value="Arabic">عربي</option>
+            </select>
           </div>
         </div>
 
-        {/* Mobile Menu Button */}
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="sm:hidden p-2 text-gray-400 hover:text-white hover:bg-green-700 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -53,19 +50,44 @@ const Navbar = () => {
         >
           <span className="sr-only">Open main menu</span>
           {isOpen ? (
-            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           ) : (
-            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
             </svg>
           )}
         </button>
       </div>
 
-      {/* Mobile Menu */}
-      <div className={`sm:hidden ${menuAnimationClasses} ${isOpen ? "block" : "hidden"} px-2 pt-2 pb-3 space-y-1`}>
+      <div
+        className={`sm:hidden ${menuAnimationClasses} ${
+          isOpen ? "block" : "hidden"
+        } px-2 pt-2 pb-3 space-y-1`}
+      >
         {NavbarItems.map((nav, index) => (
           <a
             key={index}
@@ -76,13 +98,13 @@ const Navbar = () => {
           </a>
         ))}
         <select
-            onChange={toggleMode}
-            value={mode}
-            className="block px-3 py-2  text-base font-medium bg-green-700 text-white border border-gray-300   rounded-lg focus:ring-green-500 focus:border-green-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-          >
-            <option value="Eng">English</option>
-            <option value="Arabic">عربي</option>
-          </select>
+          onChange={toggleMode}
+          value={mode}
+          className="block px-3 py-2  text-base font-medium bg-green-700 text-white border border-gray-300   rounded-lg focus:ring-green-500 focus:border-green-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+        >
+          <option value="Eng">English</option>
+          <option value="Arabic">عربي</option>
+        </select>
       </div>
     </nav>
   );
--- src/components/Home/ProductCard.tsx
+++ src/components/Home/ProductCard.tsx
@@ -6,29 +6,22 @@ interface CardItem {
 }
 
 const ProductCard: React.FC<CardItem> = ({ title, img }) => {
-  
-
   return (
     <div
-  
-      className="max-w-sm bg-white  border hover:bg-custom-color border-gray-200 rounded-lg shadow-md transition-all duration-300 ease-in-out dark:bg-gray-800 dark:border-gray-700 overflow-hidden group"
+      className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md transition-all duration-300 ease-in-out dark:bg-gray-800 dark:border-gray-700 overflow-hidden group"
     >
-      <a href="#">
+      <div className="w-full  bg-gray-200 overflow-hidden">
         <img
-          className=" px-1 rounded-tlr-xl max-w-sm "
+          className="object-cover w-full h-full"
           src={img}
           alt="Product"
         />
-      </a>
-      <div className="px-5 pb-5">
-        <a href="#">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-5 text-center">
-            {title}
-          </h5>
-        </a>
       </div>
-
-      
+      <div className="px-5 pb-5">
+        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-5 text-center">
+          {title}
+        </h5>
+      </div>
     </div>
   );
 };
